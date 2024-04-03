f23face41dc1aff3ea7fb95463925e63e55105d3
Joe Skomurski <joseph.skomurski@clearscale.com>
2024-04-03 16:09:55 -0400

    Remove unused variables

--- variables.tf
+++ variables.tf
@@ -89,42 +89,6 @@ locals {
   )
 }
 
-# variable "prefix" {
-#   type        = string
-#   description = "(Optional). Prefix override for all generated naming conventions."
-#   default     = "cs"
-# }
-
-# variable "client" {
-#   type        = string
-#   description = "(Optional). Name of the client."
-#   default     = "ClearScale"
-# }
-
-# variable "project" {
-#   type        = string
-#   description = "(Optional). Name of the client project."
-#   default     = "pmod"
-# }
-
-# variable "env" {
-#   type        = string
-#   description = "(Optional). Name of the current environment."
-#   default     = "dev"
-# }
-
-variable "region" {
-  type        = string
-  description = "(Optional). AWS region."
-  default     = "us-west-1"
-}
-
-# variable "name" {
-#   type        = string
-#   description = "(Optional). The name of the AFT deployment."
-#   default     = "default"
-# }
-
 variable "accounts" {
   description = "(Optional). List of cloud provider account info and backend type."
   type = list(object({
